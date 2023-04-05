56f88e378983b9226f869bb4443935a2f1e6fa35
Kamdzik <127319303+Kamdzik@users.noreply.github.com>
2023-04-05 12:17:42 +0000

    Initial commit

---
+++ Main.java
@@ -0,0 +1,21 @@
+class Main {
+  public static void main(String[] args) {
+
+/*
+  narysuj prostokąt
+ "******
+  ******
+  ******
+  ******
+
+  
+*/
+    for(int a = 0 ; a<4;a++){
+      System.out.println("");
+     
+      for(int b = 0 ; b<6; b++){
+       System.out.print("*");
+      }
+    }
+  }
+}
